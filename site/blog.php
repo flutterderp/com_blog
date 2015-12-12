a234b21f193163cfbc7c6eb28453dc16cee8d9b1
Michael <webmaster@southernanime.com>
2015-12-12 02:53:55 -0500

    Fix ACL violation
    Apply fix for an ACL violation in front-end views (pulled from Joomla!
    3.4.5 changes) with a small alteration—the two new error messages will
    be queued as type error rather than warning.
    
    See
    https://github.com/joomla/joomla-cms/commit/dca641f67e8e341bf0ef74f775a5
    e81ad3ccf384#diff-5 and
    https://developer.joomla.org/security-centre/630-20151003-core-acl-viola
    tions.html

--- site/blog.php
+++ site/blog.php
@@ -12,6 +12,28 @@ defined('_JEXEC') or die;
 require_once JPATH_COMPONENT . '/helpers/route.php';
 require_once JPATH_COMPONENT . '/helpers/query.php';
 
+$input	= JFactory::getApplication()->input;
+$user		= JFactory::getUser();
+
+if ($input->get('view') === 'article' && $input->get('layout') === 'pagebreak')
+{
+	if (!$user->authorise('core.edit', 'com_blog'))
+	{
+		JFactory::getApplication()->enqueueMessage(JText::_('JERROR_ALERTNOAUTHOR'), 'error');
+		
+		return;
+	}
+}
+elseif ($input->get('view') === 'articles' && $input->get('layout') === 'modal')
+{
+	if (!$user->authorise('core.edit', 'com_blog'))
+	{
+		JFactory::getApplication()->enqueueMessage(JText::_('JERROR_ALERTNOAUTHOR'), 'error');
+		
+		return;
+	}
+}
+
 $controller = JControllerLegacy::getInstance('Blog');
-$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->execute($input->get('task'));
 $controller->redirect();
