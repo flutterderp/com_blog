ae8bd6fdaff13f836a65f1988c5c696976ae9dc2
Otterly Useless <michael@hendrixweb.net>
2021-09-22 14:50:40 -0400

    Update 3.0.26.5.sql

--- admin/sql/updates/mysql/3.0.26.5.sql
+++ admin/sql/updates/mysql/3.0.26.5.sql
@@ -3,8 +3,9 @@ MODIFY COLUMN `extra1` varchar(200) NOT NULL DEFAULT '' COMMENT 'Placeholder for
 MODIFY COLUMN `extra2` varchar(200) NOT NULL DEFAULT '' COMMENT 'Placeholder for additional custom field.' AFTER `extra1`,
 MODIFY COLUMN `extra3` varchar(200) NOT NULL DEFAULT '' COMMENT 'Placeholder for additional custom field.' AFTER `extra2`,
 MODIFY COLUMN `attribs` text NOT NULL AFTER `urls`,
-DROP KEY `idx_introtext`,
-DROP KEY `idx_fulltext`,
-DROP KEY `idx_sources`,
-ADD KEY `idx_introtext` (`introtext`(191)), -- 768 / 4 (https://dev.mysql.com/doc/refman/8.0/en/innodb-limits.html)
-ADD KEY `idx_fulltext` (`fulltext`(191)); -- 768 / 4
+DROP KEY `idx_sources`;
+
+-- May or may need these later?
+-- ALTER TABLE `#__blog`
+-- ADD KEY `idx_introtext` (`introtext`(191)), -- 768 / 4 (https://dev.mysql.com/doc/refman/8.0/en/innodb-limits.html)
+-- ADD KEY `idx_fulltext` (`fulltext`(191)); -- 768 / 4
