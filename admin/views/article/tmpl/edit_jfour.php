b007a892b19664eb2b72c9e0b7d593e70fb154cd
Otterly Useless <michael@hendrixweb.net>
2022-06-30 10:26:03 -0400

    J4: article text not saving

--- admin/views/article/tmpl/edit_jfour.php
+++ admin/views/article/tmpl/edit_jfour.php
@@ -10,12 +10,11 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Associations;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Version;
 use Joomla\Registry\Registry;
 
 // Include the component HTML helpers.
@@ -23,16 +22,17 @@ use Joomla\Registry\Registry;
 
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $this->document->getWebAssetManager();
-// $wa->getRegistry()->addExtensionRegistryFile('com_contenthistory');
+$wa->getRegistry()->addExtensionRegistryFile('com_contenthistory');
 $wa->useScript('keepalive')
-	->useScript('form.validate');
-	// ->useScript('com_contenthistory.admin-history-versions');
-
-$this->useCoreUI = true;
+	->useScript('form.validate')
+	->useScript('com_contenthistory.admin-history-versions');
 
 $this->configFieldsets  = array('editorConfig');
 $this->hiddenFieldsets  = array('basic-limited');
-$this->ignore_fieldsets = array('jmetadata', 'item_associations');
+$fieldsetsInImages = ['image-intro', 'image-full'];
+$fieldsetsInLinks = ['linka', 'linkb', 'linkc'];
+$this->ignore_fieldsets = array_merge(array('jmetadata', 'item_associations'), $fieldsetsInImages, $fieldsetsInLinks);
+$this->useCoreUI = true;
 
 // Create shortcut to parameters.
 $params = clone $this->state->get('params');
@@ -41,29 +41,42 @@ $params->merge(new Registry($this->item->attribs));
 $app   = Factory::getApplication();
 $doc   = Factory::getDocument();
 $input = $app->input;
-$assoc = Associations::isEnabled();
 
-if (!$assoc)
+$assoc              = Associations::isEnabled();
+$showArticleOptions = $params->get('show_article_options', 1);
+
+if (!$assoc || !$showArticleOptions)
 {
 	$this->ignore_fieldsets[] = 'frontendassociations';
 }
 
+if (!$showArticleOptions)
+{
+	// Ignore fieldsets inside Options tab
+	$this->ignore_fieldsets = array_merge($this->ignore_fieldsets, ['attribs', 'basic', 'category', 'author', 'date', 'other']);
+}
+
 // In case of modal
-$isModal = $input->get('layout') == 'modal' ? true : false;
+$isModal = $input->get('layout') === 'modal';
 $layout  = $isModal ? 'modal' : 'edit';
 $tmpl    = $isModal || $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=component' : '';
 ?>
 
-<form enctype="multipart/form-data" action="<?php echo JRoute::_('index.php?option=com_blog&layout=' . $layout . $tmpl . '&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="item-form" class="form-validate">
-	<?php echo JLayoutHelper::render('joomla.edit.title_alias', $this); ?>
+<form action="<?php echo Route::_('index.php?option=com_blog&layout=' . $layout . $tmpl . '&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="item-form" aria-label="<?php echo Text::_('COM_CONTENT_FORM_TITLE_' . ((int) $this->item->id === 0 ? 'NEW' : 'EDIT'), true); ?>" class="form-validate">
+	<?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
 
 	<div class="main-card">
 		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'general', 'recall' => true, 'breakpoint' => 768]); ?>
 
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'general', empty($this->item->id) ? Text::_('COM_BLOG_NEW_ARTICLE', true) : Text::sprintf('COM_BLOG_EDIT_ARTICLE', $this->item->id, true)); ?>
+		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'general', Text::_('COM_BLOG_ARTICLE_CONTENT')); ?>
 		<div class="row">
 			<div class="col-lg-9">
-				<div class="control-group"><?php echo $this->form->getInput('articletext'); ?></div>
+				<div>
+					<fieldset class="adminform">
+						<?php echo $this->form->getLabel('articletext'); ?>
+						<?php echo $this->form->getInput('articletext'); ?>
+					</fieldset>
+				</div>
 
 				<div class="form-inline form-inline-header"><?php echo $this->form->renderField('video_uri'); ?></div>
 
@@ -75,11 +88,11 @@ $tmpl    = $isModal || $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=c
 
 				<?php echo $this->form->renderField('sources_blob'); ?>
 			</div>
-
 			<div class="col-lg-3">
-				<?php echo LayoutHelper::render('joomla.edit.global', $this, null, array('client' => 1)); ?>
+				<?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
 			</div>
 		</div>
+
 		<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
 		<?php // Do not show the images and links options if the edit form is configured not to. ?>
@@ -87,21 +100,29 @@ $tmpl    = $isModal || $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=c
 			<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'images', Text::_('COM_BLOG_FIELDSET_URLS_AND_IMAGES')); ?>
 			<div class="row">
 				<div class="col-12 col-lg-6">
-					<?php echo $this->form->renderField('images'); ?>
-					<?php foreach ($this->form->getGroup('images') as $field) : ?>
-						<?php echo $field->renderField(); ?>
-					<?php endforeach; ?>
+				<?php foreach ($fieldsetsInImages as $fieldset) : ?>
+					<fieldset id="fieldset-<?php echo $fieldset; ?>" class="options-form">
+						<legend><?php echo Text::_($this->form->getFieldsets()[$fieldset]->label); ?></legend>
+						<div>
+						<?php echo $this->form->renderFieldset($fieldset); ?>
+						</div>
+					</fieldset>
+				<?php endforeach; ?>
 				</div>
 				<div class="col-12 col-lg-6">
-					<?php foreach ($this->form->getGroup('urls') as $field) : ?>
-						<?php echo $field->renderField(); ?>
-					<?php endforeach; ?>
+				<?php foreach ($fieldsetsInLinks as $fieldset) : ?>
+					<fieldset id="fieldset-<?php echo $fieldset; ?>" class="options-form">
+						<legend><?php echo Text::_($this->form->getFieldsets()[$fieldset]->label); ?></legend>
+						<div>
+						<?php echo $this->form->renderFieldset($fieldset); ?>
+						</div>
+					</fieldset>
+				<?php endforeach; ?>
 				</div>
 			</div>
 			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 		<?php endif; ?>
 
-		<?php $this->show_options = $params->get('show_article_options', 1); ?>
 		<?php echo LayoutHelper::render('joomla.edit.params', $this); ?>
 
 		<?php // Do not show the publishing options if the edit form is configured not to. ?>
@@ -112,7 +133,7 @@ $tmpl    = $isModal || $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=c
 					<fieldset id="fieldset-publishingdata" class="options-form">
 						<legend><?php echo Text::_('JGLOBAL_FIELDSET_PUBLISHING'); ?></legend>
 						<div>
-							<?php echo LayoutHelper::render('joomla.edit.publishingdata', $this); ?>
+						<?php echo LayoutHelper::render('joomla.edit.publishingdata', $this); ?>
 						</div>
 					</fieldset>
 				</div>
@@ -120,7 +141,7 @@ $tmpl    = $isModal || $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=c
 					<fieldset id="fieldset-metadata" class="options-form">
 						<legend><?php echo Text::_('JGLOBAL_FIELDSET_METADATA_OPTIONS'); ?></legend>
 						<div>
-							<?php echo LayoutHelper::render('joomla.edit.metadata', $this); ?>
+						<?php echo LayoutHelper::render('joomla.edit.metadata', $this); ?>
 						</div>
 					</fieldset>
 				</div>
@@ -128,31 +149,52 @@ $tmpl    = $isModal || $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=c
 			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 		<?php endif; ?>
 
-		<?php if ( ! $isModal && $assoc) : ?>
+		<?php if (!$isModal && $assoc && $params->get('show_associations_edit', 1) == 1) : ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'associations', Text::_('JGLOBAL_FIELDSET_ASSOCIATIONS')); ?>
-			<?php echo $this->loadTemplate('associations'); ?>
+			<fieldset id="fieldset-associations" class="options-form">
+			<legend><?php echo Text::_('JGLOBAL_FIELDSET_ASSOCIATIONS'); ?></legend>
+			<div>
+			<?php echo LayoutHelper::render('joomla.edit.associations', $this); ?>
+			</div>
+			</fieldset>
 			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 		<?php elseif ($isModal && $assoc) : ?>
-			<div class="hidden"><?php echo $this->loadTemplate('associations'); ?></div>
+			<div class="hidden"><?php echo LayoutHelper::render('joomla.edit.associations', $this); ?></div>
 		<?php endif; ?>
 
-		<?php if ($this->canDo->get('core.admin')) : ?>
+		<?php if ($this->canDo->get('core.admin') && $params->get('show_configure_edit_options', 1) == 1) : ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'editor', Text::_('COM_BLOG_SLIDER_EDITOR_CONFIG')); ?>
-			<?php echo $this->form->renderFieldset('editorConfig'); ?>
+			<fieldset id="fieldset-editor" class="options-form">
+				<legend><?php echo Text::_('COM_BLOG_SLIDER_EDITOR_CONFIG'); ?></legend>
+				<div class="form-grid">
+				<?php echo $this->form->renderFieldset('editorConfig'); ?>
+				</div>
+			</fieldset>
 			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 		<?php endif; ?>
 
-		<?php if ($this->canDo->get('core.admin')) : ?>
+		<?php if ($this->canDo->get('core.admin') && $params->get('show_permissions', 1) == 1) : ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'permissions', Text::_('COM_BLOG_FIELDSET_RULES')); ?>
+			<fieldset id="fieldset-rules" class="options-form">
+				<legend><?php echo Text::_('COM_BLOG_FIELDSET_RULES'); ?></legend>
+				<div>
 				<?php echo $this->form->getInput('rules'); ?>
+				</div>
+			</fieldset>
 			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 		<?php endif; ?>
 
 		<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 
-		<input type="hidden" name="task" value="" />
-		<input type="hidden" name="return" value="<?php echo $input->get('return', null, 'BASE64'); ?>" />
-		<input type="hidden" name="forcedLanguage" value="<?php echo $input->get('forcedLanguage', '', 'cmd'); ?>" />
+		<?php // Creating 'id' hiddenField to cope with com_associations sidebyside loop ?>
+		<?php if ($params->get('show_publishing_options', 1) == 0) : ?>
+			<?php $hidden_fields = $this->form->getInput('id'); ?>
+			<div class="hidden"><?php echo $hidden_fields; ?></div>
+		<?php endif; ?>
+
+		<input type="hidden" name="task" value="">
+		<input type="hidden" name="return" value="<?php echo $input->getBase64('return'); ?>">
+		<input type="hidden" name="forcedLanguage" value="<?php echo $input->get('forcedLanguage', '', 'cmd'); ?>">
 		<?php echo HTMLHelper::_('form.token'); ?>
 	</div>
 </form>
